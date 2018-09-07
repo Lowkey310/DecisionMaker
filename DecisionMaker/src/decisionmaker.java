bb057b910e0850b805af7a15db54569db2ad51c0
tianyu.zhou <tianyu.zhou@mu-c1-17.mullen.pvt.k12.co.us>
2018-09-07 14:55:35 -0600

    decision1

---
+++ DecisionMaker/src/decisionmaker.java
@@ -0,0 +1,17 @@
+
+public class decisionmaker
+	{
+
+		public static void main(String[] args)
+			{
+				challenge();
+
+			}
+
+		private static void challenge()
+			{
+				System.out.println("");
+				
+			}
+
+	}
