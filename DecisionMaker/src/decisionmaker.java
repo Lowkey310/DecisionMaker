e57dfc0311035aac98636002947c4fd3a46b45eb
zhoutianyu <zhoutianyu@192.168.14.186>
2018-09-11 08:45:48 -0600

    finaldecisionmaker

--- DecisionMaker/src/decisionmaker.java
+++ DecisionMaker/src/decisionmaker.java
@@ -1,17 +1,55 @@
+import java.util.Scanner;
 
 public class decisionmaker
 	{
 
 		public static void main(String[] args)
 			{
-				challenge();
+				Intro();
+				makingchoicesprocess();
+				asktorunagain();
+				
 
 			}
-
-		private static void challenge()
+		
+		private static void Intro()
 			{
-				System.out.println("");
+				System.out.println("Hi! Nice to meet you! I'm your private decisionmaker!");
+			}
+
+		private static void makingchoicesprocess()
+			{
+				System.out.println("I know it's hard to make a decision, so now let me help u!");
+				System.out.println("How many choices do you got for me?");
+				Scanner userInput = new Scanner(System.in);
+				int choices = userInput.nextInt();
+				String[] content = new String [choices];
+				for(int i = 0; i < choices; i++)
+					{
+						System.out.println("What is your choice #" + (i+1));
+						Scanner userInput1 = new Scanner(System.in);
+						String answer = userInput1.nextLine();
+						content [i] = answer;
+						
+					}
+				System.out.println("I see! You should absoulutely choose " + content [(int) (Math.random()*choices)]+ "!");
+			}
+		
+		private static void asktorunagain()
+			{
+				System.out.println("Well, Do you have any other decision to make? 1. Yea Sure! I still need your help! 2. Nope I'm outta here. Remember to type in the number infront of your choice !");
+				Scanner userInput3 = new Scanner(System.in);
+				int answerafterward = userInput3.nextInt();
+				if (answerafterward == 1 )
+					{
+						System.out.println("Welcome Back! I miss you already!");
+						makingchoicesprocess();
+	
+					}
+				else
+					{
+						System.out.println("Alright, see you soon T^T");
+					}
 				
 			}
-
 	}
